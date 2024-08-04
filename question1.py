1dfea9db0168f066f209a76e1843119a080026e7
Chinmayee Mali <138202316+ChinmayeeMali@users.noreply.github.com>
2024-08-04 23:23:30 +0530

    Add files via upload

---
+++ question1.py
@@ -0,0 +1,5 @@
+numerator=100
+denominator=0
+if(denominator==0):
+    result=float('inf')
+print(result)
---
+++ question2.py
@@ -0,0 +1,6 @@
+list=["apple","banana","cherry","orange"]
+count=0
+for x in list:
+    if(x=="orange"):
+        print(count)
+    count=count+1
